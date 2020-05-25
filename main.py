49843b4fe305512c1b78c99357e66a1d5eaf03cc
Delanary <fedor.rybochkin@gmail.com>
2020-05-25 20:27:15 +0300

    Production release

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+from config import token, login, chat
+
 import vk_api
-vk_session = vk_api.VkApi('+71234567890', 'mypassword')
-vk_session.auth()
+from random import randrange
+import datetime
+import time
+
+
+def main():
+    while True:
+        try:
+            if datetime.datetime.now().hour != 15:
+                time.sleep(60 * 60)
+                print(datetime.datetime.now().hour)
+                continue
+            vk_session = vk_api.VkApi(token=token)
+            vk = vk_session.get_api()
+            poll = vk.polls.create(
+                question="Фамия, %s" % str(datetime.datetime.now().day) + str(datetime.datetime.now().month) + str(
+                    datetime.datetime.now().year), is_multiple=1,
+                add_answers="[\"21:00\", \"21:30\", \"22:00\", \"22:30\", \"Позже\", \"хз\", \"Нет\"]")
+            poll_att = "poll{}_{}".format(poll['owner_id'], poll['id'])
+            vk.messages.send(peer_id=2000000000 + chat, message="", random_id=randrange(10 ** 9), attachment=poll_att)
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    main()
