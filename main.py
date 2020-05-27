a94cc373ed2961b8915b45cd180919b6b2989685
Delanary <fedor.rybochkin@gmail.com>
2020-05-27 18:25:15 +0300

    Pinning polls

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import vk_api
 from random import randrange
 import datetime
 import time
+import sys
 
 
 def captcha_handler(captcha):
-
     key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
 
     return captcha.try_again(key)
@@ -17,7 +17,7 @@ def captcha_handler(captcha):
 def main():
     while True:
         try:
-            if datetime.datetime.now().hour != 15:
+            if datetime.datetime.now().hour != 15 and not sys.argv[1] == "test":
                 print(datetime.datetime.now().hour)
                 time.sleep(60 * 60)
                 continue
@@ -27,7 +27,14 @@ def main():
                 question="Фамия, %s" % str(datetime.datetime.now()), is_multiple=1,
                 add_answers="[\"21:00\", \"21:30\", \"22:00\", \"22:30\", \"Позже\", \"хз\", \"Нет\"]")
             poll_att = "poll{}_{}".format(poll['owner_id'], poll['id'])
-            vk.messages.send(peer_id=2000000000 + chat, message="", random_id=randrange(10 ** 9), attachment=poll_att)
+            id = randrange(10 ** 9)
+            message_id = vk.messages.send(peer_id=2000000000 + chat, message="", random_id=id, attachment=poll_att)
+            print(id)
+            try:
+                vk.messages.pin(peer_id=2000000000 + chat, message_id=message_id)
+            except Exception as e:
+                print(e)
+                pass
             time.sleep(60 * 60)
         except KeyboardInterrupt as e:
             print('Get interrupt signal, stopping')
